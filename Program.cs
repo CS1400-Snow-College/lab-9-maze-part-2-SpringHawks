c6f252b138de34d73836425600edcffe7bed388a
Annette Hawks <hawksannette@gmail.com>
2025-07-16 23:07:02 -0600

    Rules

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿// Annette Hawks
 // Due 7/16/2025
-// Lab 8 Maze. 
+// Lab 8 Maze 
+
+//Rules &Introduction ish
+  Console.WriteLine("MAZE RULES:\n" +
+                          "1. Use arrow keys to move.\n" +
+                          "2. Walls are '*'.\n" +
+                          "3. Reach the exit, '#' to win.\n" +
+                          "4. Bad Guys: '%'(move) and '&'(static).\n" +
+                          "5. Coins: '^' worth 100 points.\n" +
+                          "6. Gems: '$' worth 200 points.\n" +
+                          "Press any key to start...");
+        Console.ReadKey(true);
+        Console.Clear();
